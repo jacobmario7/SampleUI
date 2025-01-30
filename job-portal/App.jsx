1d4e299daf93987855750ad7f548b71becbf722b
Jacob Mario Leonard P <160170771+jacobmario7@users.noreply.github.com>
2025-01-30 13:21:30 +0530

    Update App.jsx

--- job-portal/App.jsx
+++ job-portal/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Sidebar from './components/Sidebar';
+import Sidebar from './components/SideBar';
 import JobList from './components/JobList';
 import TopBar from './components/TopBar';
 
